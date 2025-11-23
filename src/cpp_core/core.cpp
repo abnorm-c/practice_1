900271819e01c7a535899fb82bfd94a4fc1ba6a7
abnorm-c <zimanov02@mail.ru>
2025-11-24 00:59:06 +0500

    fix

--- src/cpp_core/core.cpp
+++ src/cpp_core/core.cpp
@@ -2,13 +2,16 @@
 #include <fstream>
 #include <cmath>
 #include <vector>
+#include "json.hpp"
 
 #define M_PI 3.14159265358979323846
+
+using json = nlohmann::json;
 
 void calculate_well(
     double h, double fi, double k, double mu, double c, 
     double p_0, double r_w, const double* pressure_profile,
-    double* time_result, double* flow_result, double* kf_result,
+    double* time_days, double* flow_rate_m3_day, double* kf_result,
     int N
 ) {
     double dt = 86400.0;
@@ -35,8 +38,9 @@ void calculate_well(
     double Ei0 = -expi_approx(B);
     double A = 4.0 * kf * M_PI * fi * c * h / Ei0;
     
+	std::vector<double> time_seconds(N);
     for (int i = 0; i < N; i++) {
-        time_result[i] = (i + 1) * dt;
+        time_seconds[i] = (i + 1) * dt;
     }
     
     std::vector<double> E(N);
@@ -47,50 +51,73 @@ void calculate_well(
         E[n] = (Ei_n - Ei_n1) / Ei0;
     }
     
-
-    flow_result[0] = A * (p_0 - pressure_profile[0]);
+    std::vector<double> flow_rate_m3_sec(N);
+    flow_rate_m3_sec[0] = A * (p_0 - pressure_profile[0]);
     
     for (int n = 1; n < N; n++) {
         double sum_term = 0.0;
         for (int i = 0; i < n; i++) {
-            sum_term += E[i] * flow_result[n - 1 - i];
+            sum_term += E[i] * flow_rate_m3_sec[n - 1 - i];
         }
-        flow_result[n] = A * (p_0 - pressure_profile[n]) + sum_term;
+        flow_rate_m3_sec[n] = A * (p_0 - pressure_profile[n]) + sum_term;
+    }
+    
+    for (int i = 0; i < N; i++) {
+        time_days[i] = time_seconds[i] / 86400.0;  
+        flow_rate_m3_day[i] = flow_rate_m3_sec[i] * 86400.0; 
     }
 }
 
 int main() {
     try {
-        double h = 20.0, fi = 0.15, k = 1e-15, mu = 1e-3, c = 0.5e-9;
-        double p_0 = 250e5, r_w = 0.12;
-        int N = 300;
+       std::ifstream input("input_params.json");
+        if (!input) {
+            std::cerr << "n/a" << std::endl;
+            return 1;
+        }
+        
+        json params;
+        input >> params;
+        input.close();
+
+        double h = params["height"];
+        double fi = params["porosity"];
+        double k = params["permeability"];
+        double mu = params["viscosity"];
+        double c = params["compressibility"];
+        double p_0 = params["initial_pressure"];
+        double r_w = params["well_radius"];
+        double p_zab = params["bottomhole_pressure"];
+        int N = params["points_count"];
+        
+        std::cout << "ПРОЧИТАНЫ ПАРАМЕТРЫ ИЗ JSON:" << std::endl;
+        std::cout << "k=" << k << " м²" << std::endl;
         
         std::vector<double> pressure_profile(N);
         for(int i = 0; i < 50; i++) pressure_profile[i] = p_0;
-        for(int i = 50; i < N; i++) pressure_profile[i] = 150e5;
+        for(int i = 50; i < N; i++) pressure_profile[i] = p_zab;
         
-        std::vector<double> time_result(N);
-        std::vector<double> flow_result(N);
+        std::vector<double> time_days(N);
+        std::vector<double> flow_rate_m3_day(N);
         double kf_result;
         
         calculate_well(h, fi, k, mu, c, p_0, r_w, pressure_profile.data(), 
-                       time_result.data(), flow_result.data(), &kf_result, N);
-        std::ofstream out("result.txt");
-        out << kf_result << std::endl;
+                       time_days.data(), flow_rate_m3_day.data(), &kf_result, N);
         
-        for(int i = 0; i < N; i++) {
-            out << time_result[i] << " " << flow_result[i] << std::endl;
-        }
+		json results;
+        results["piezometric_coefficient"] = kf_result;
+        results["time_days"] = time_days;
+        results["flow_rate_m3_day"] = flow_rate_m3_day;
+        
+        std::ofstream out("result.json");
+        out << results.dump(2);
         out.close();
         
-        std::cout << "Done" << std::endl;
+        std::cout << "result.json is ready" << std::endl;
         return 0;
         
     } catch (const std::exception& e) {
-        std::cerr << "Ошибка: " << e.what() << std::endl;
-        return 1;
-    } catch (...) {
-        std::cerr << "Неизвестная ошибка" << std::endl;
+        std::cerr << "Error: " << e.what() << std::endl;
         return 1;
     }
 }
